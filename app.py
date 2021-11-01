bf7bcb174e9ba4d072b99f57d0d3fcab136e7ec8
Wei-ju Wu <weiju.wu@gmail.com>
2021-11-01 14:25:36 -0700

    first route for microarray data

--- app.py
+++ app.py
@@ -14,15 +14,42 @@ app = Flask(__name__)
 CORS(app)
 app.config.from_envvar('APP_SETTINGS')
 
+HALO_ID = 4
 
 def dbconn(dbname):
-    return  pymssql.connect(server=app.config('DBSERVER'), port=1433,
-                            user=app.config('DBUSER'), password=app.config('DBPASS'),
+    return  pymssql.connect(server=app.config['DBSERVER'], port=1433,
+                            user=app.config['DBUSER'], password=app.config['DBPASS'],
                             database=dbname)
 
 @app.route('/')
 def index():
     return "<p>Hello !</p>"
+
+
+@app.route('/microarray_data/<gene>')
+def microarray_data(gene):
+    conn = dbconn('microarray3')
+    cursor = conn.cursor()
+    cursor.execute('select cc.condition_name,ge.common_name,ge.canonical_name,log10_ratio,lambda from gene_expression ge join comparison_condition cc on ge.condition_id=cc.condition_id where canonical_name=%s', gene)
+    result = []
+    for cond_name,common_name,canonical_name,log10,lambdaval in cursor.fetchall():
+        result.append({'condition': cond_name, 'common_name': common_name,
+                       'canonical_name': canonical_name, 'log10_ratio': log10,
+                       'lambda': lambdaval})
+
+    return jsonify(expressions=result, num_expressions=len(result))
+
+
+@app.route('/genes')
+def genes():
+    conn = dbconn('microarray3')
+    cursor = conn.cursor()
+    cursor.execute('select distinct biosequence_name from biosequence where organism_id=%d' % HALO_ID)
+    result = []
+    for row in cursor.fetchall():
+        result.append(row[0])
+
+    return jsonify(genes=result, num_genes=len(result))
 
 
 if __name__ == '__main__':
