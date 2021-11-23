fa734af0137f60b370972ef0a1b47b91ee5e3dd9
Wei-ju Wu <weiju.wu@gmail.com>
2021-11-23 14:49:46 -0800

    implemented basic search

--- client2/src/components/SearchForm.vue
+++ client2/src/components/SearchForm.vue
@@ -5,29 +5,67 @@
       <v-col class="mb-4">
         <h2 class="display-2 font-weight-bold mb-3">Gene Search</h2>
         <div>
-          <v-form ref="form" v-model="valid"
-                  lazy-validation>
+          <v-form ref="form" @submit.prevent>
             <v-text-field
               v-model="gene"
-              :counter="10"
               label="Search (e.g. VNG00001)"
-              required>
+              @keydown.enter="onSubmit">
             </v-text-field>
           </v-form>
         </div>
 
       </v-col>
-
+    </v-row>
+    <v-row v-if="annotations.length" class="text-center">
+      <h2>Results</h2>
+    </v-row>
+    <v-row v-if="annotations.length" class="text-center">
+      <v-col class="mb-4">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-left">Aliases</th>
+                <th class="text-left">Functional Description</th>
+                <th class="text-left">Gene Symbol</th>
+                <th class="text-left">Location</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in annotations" :key="item.full_gene_name">
+                <td>{{item.aliases}}</td>
+                <td>{{item.functional_description}}</td>
+                <td>{{item.gene_symbol}}</td>
+                <td>{{item.location}}</td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+  var BASE_URL = 'http://localhost:5000';
   export default {
     name: 'SearchForm',
 
-  data: () => ({
-  gene: ''
+    data: () => ({
+        gene: '',
+        annotations: []
     }),
+    methods: {
+        onSubmit: function() {
+            var api = BASE_URL + '/annotations/' + this.gene;
+            console.log('submitting: ' + api);
+            fetch(api)
+                .then((response) => { return response.json();
+                                    }).then((result) => {
+                                        this.annotations = result.annotations;
+                });
+      }
+    }
   }
 </script>
