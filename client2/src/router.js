03410f352fc8692d931c604c3f6fadb5e2155734
Wei-ju Wu <weiju.wu@gmail.com>
2021-12-16 12:58:21 -0800

    moved Gene view

--- client2/src/router.js
+++ client2/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import SearchForm from './components/SearchForm'
+import GeneView from './components/GeneView'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
             path: '/',
             name: 'home',
             component: SearchForm
+        },
+        {
+            path: '/viewgene/:gene',
+            name: 'viewgene',
+            component: GeneView
         }
     ]
 });
