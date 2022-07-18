eb4b90b8a00a1a6998693f7bdf2df19d21afe43f
Wei-ju Wu <weiju.wu@gmail.com>
2022-07-18 14:45:15 -0700

    changed affiliation information on about page

--- client2/src/components/AboutPage.vue
+++ client2/src/components/AboutPage.vue
@@ -287,30 +287,29 @@ VNG_7025,VNG_7026,VNG_7027,VNG_7028
         <p>&nbsp;</p>
         <h2 id="team">Team</h2>
         <ul>
-          <li>Alan P. R. Lorenzetti <sup><a href="#fn1">1</a>,<a href="#fn2">2</a>,<a href="#fn3">3</a></sup></li>
+          <li>Alan P. R. Lorenzetti <sup><a href="#fn1">1</a>,<a href="#fn2">2</a></sup></li>
           <li>Ulrike Kusebauch <sup><a href="#fn2">2</a></sup></li>
           <li>Lívia S. Zaramela <sup><a href="#fn1">1</a></sup></li>
           <li>Wei-Ju Wu <sup><a href="#fn2">2</a></sup></li>
-          <li>João P. P. de Almeida <sup><a href="#fn1">1</a>,<a href="#fn4">4</a></sup></li>
+          <li>João P. P. de Almeida <sup><a href="#fn1">1</a>,<a href="#fn3">3</a></sup></li>
           <li>Serdar Turkarslan <sup><a href="#fn2">2</a></sup></li>
-          <li>Adrián L. G. de Lomana <sup><a href="#fn2">2</a>,<a href="#fn5">5</a></sup></li>
-          <li>José V. Gomes-Filho <sup><a href="#fn1">1</a>,<a href="#fn6">6</a></sup></li>
-          <li>Ricardo Z. N. Vêncio <sup><a href="#fn7">7</a></sup></li>
+          <li>Adrián L. G. de Lomana <sup><a href="#fn2">2</a>,<a href="#fn4">4</a></sup></li>
+          <li>José V. Gomes-Filho <sup><a href="#fn1">1</a>,<a href="#fn5">5</a></sup></li>
+          <li>Ricardo Z. N. Vêncio <sup><a href="#fn6">6</a></sup></li>
           <li>Robert L. Moritz <sup><a href="#fn2">2</a></sup></li>
           <li>Tie Koide <sup><a href="#fn1">1</a></sup></li>
-          <li>Nitin S. Baliga <sup><a href="#fn1">2</a>,<a href="#fn8">8</a>,<a href="#fn9">9</a>,<a href="#fn10">10</a></sup></li>
+          <li>Nitin S. Baliga <sup><a href="#fn1">2</a>,<a href="#fn7">7</a>,<a href="#fn8">8</a>,<a href="#fn9">9</a></sup></li>
         </ul>
         <hr style="margin-top: 10pt; margin-bottom: 10pt" />
         <sup id="fn1">1. Department of Biochemistry and Immunology, Ribeirão Preto Medical School, University of São Paulo, Ribeirão Preto, Brazil</sup><br/>
         <sup id="fn2">2. Institute for Systems Biology, Seattle, WA, USA</sup><br/>
-        <sup id="fn3">3. Present address: Lieber Institute for Brain Development, Baltimore, MD, USA</sup><br/>
-        <sup id="fn4">4. Present address: Institute of Biological Sciences, Federal University of Minas Gerais, Belo Horizonte, Brazil</sup><br/>
-        <sup id="fn5">5. Present address: Center for Systems Biology, University of Iceland, Reykjavik, Iceland</sup><br/>
-        <sup id="fn6">6. Present address: Prokaryotic RNA Biology, Phillips-Universität Marburg, Marburg, Germany</sup><br/>
-        <sup id="fn7">7. Department of Computation and Mathematics, Faculty of Philosophy, Sciences and Letters at Ribeirão Preto, University of São Paulo, Ribeirão Preto, Brazil</sup><br/>
-        <sup id="fn8">8. Departments of Biology and Microbiology, University of Washington, Seattle, WA, USA</sup><br/>
-        <sup id="fn9">9. Molecular and Cellular Biology Program, University of Washington, Seattle, WA, USA</sup><br/>
-        <sup id="fn10">10. Lawrence Berkeley National Lab, Berkeley, CA, USA</sup><br/>
+        <sup id="fn3">3. Present address: Institute of Biological Sciences, Federal University of Minas Gerais, Belo Horizonte, Brazil</sup><br/>
+        <sup id="fn4">4. Present address: Center for Systems Biology, University of Iceland, Reykjavik, Iceland</sup><br/>
+        <sup id="fn5">5. Present address: Prokaryotic RNA Biology, Phillips-Universität Marburg, Marburg, Germany</sup><br/>
+        <sup id="fn6">6. Department of Computation and Mathematics, Faculty of Philosophy, Sciences and Letters at Ribeirão Preto, University of São Paulo, Ribeirão Preto, Brazil</sup><br/>
+        <sup id="fn7">7. Departments of Biology and Microbiology, University of Washington, Seattle, WA, USA</sup><br/>
+        <sup id="fn8">8. Molecular and Cellular Biology Program, University of Washington, Seattle, WA, USA</sup><br/>
+        <sup id="fn9">9. Lawrence Berkeley National Lab, Berkeley, CA, USA</sup><br/>
       </v-col>
     </v-row>
   </v-container>
